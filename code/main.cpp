8c25ae690dc0909e456487b742006c84c8bd2d62
Aiden <marcin.madry@interia.com>
2022-10-31 21:31:23 +0100

    Remove CAMERA_PTR macro, clamp zooming in and out, introduce gl_render

--- code/main.cpp
+++ code/main.cpp
@@ -8,7 +8,10 @@
 #define UNUSED(x) ((void)(x))
 #define WIDTH 1280
 #define HEIGHT 720
+
 #define SCALE_FACTOR 0.05f
+#define SCALE_MAX 10.0f
+#define SCALE_MIN 0.1f
 
 #define global static
 #define internal static
@@ -16,9 +19,6 @@
 #define QUAD_VERTICES 4
 #define QUAD_TRIANGLES 2
 #define QUAD_ELEMENTS 3
-
-// TODO(Aiden): This is a macro for now
-#define CAMERA_PTR(window) (&(((Renderer *) glfwGetWindowUserPointer((window)))->camera))
 
 struct Vec2
 {
@@ -48,6 +48,9 @@ struct Renderer
 {
     Vec2 vertices[QUAD_VERTICES];
     Triangle indices[QUAD_TRIANGLES];
+
+    unsigned int VAO;
+    unsigned int VBO;
     
     Camera camera;
 };
@@ -95,16 +98,24 @@ internal void immediate_quad_centered(Renderer *renderer, float x, float y, floa
     renderer->indices[1] = { 1, 2, 3 };
 }
 
+internal void gl_render(Renderer *renderer)
+{
+    glBindVertexArray(renderer->VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, renderer->VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(renderer->vertices), renderer->vertices);
+    glDrawElements(GL_TRIANGLES, QUAD_TRIANGLES * QUAD_ELEMENTS, GL_UNSIGNED_INT, renderer->indices);
+}
+
 internal void cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
 {
     int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
-    Camera *camera = CAMERA_PTR(window);
+    Camera *camera = static_cast<Camera *> (glfwGetWindowUserPointer(window));
     
     if (state == GLFW_PRESS) {
         camera->offset_x -= (static_cast<float> (xpos) - camera->mouse_x) / camera->scale;
         camera->offset_y -= (static_cast<float> (ypos) - camera->mouse_y) / camera->scale;
-    } 
-
+    }
+    
     camera->mouse_x = static_cast<float> (xpos);
     camera->mouse_y = static_cast<float> (ypos);
 }
@@ -113,15 +124,17 @@ internal void scroll_callback(GLFWwindow *window, double xoffset, double yoffset
 {
     UNUSED(xoffset);
     
-    Camera *camera = CAMERA_PTR(window);
+    Camera *camera = static_cast<Camera *> (glfwGetWindowUserPointer(window));
     
     float before_x, before_y;
     screen_to_world(camera, camera->mouse_x, camera->mouse_y, &before_x, &before_y);
     
     if (yoffset < 0.0) {
         camera->scale *= (1.0f - SCALE_FACTOR);
+        if (camera->scale < SCALE_MIN) camera->scale = SCALE_MIN;
     } else if (yoffset > 0.0) {
         camera->scale *= (1.0f + SCALE_FACTOR);
+        if (camera->scale > SCALE_MAX) camera->scale = SCALE_MAX;
     }
 
     float after_x, after_y;
@@ -193,15 +206,14 @@ int main(int argc, char **argv)
     glUniform2f(glGetUniformLocation(shader_program, "resolution"), WIDTH, HEIGHT);
     
     // Render
-    unsigned int VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    
     Renderer renderer = {0};
     
+    glGenVertexArrays(1, &renderer.VAO);
+    glGenBuffers(1, &renderer.VBO);
+
+    glBindVertexArray(renderer.VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, renderer.VBO);
+        
     glBufferData(GL_ARRAY_BUFFER, sizeof(renderer.vertices), renderer.vertices, GL_DYNAMIC_DRAW);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vec2), (void *) offsetof(Renderer, vertices));
     glEnableVertexAttribArray(0);
@@ -214,25 +226,22 @@ int main(int argc, char **argv)
     renderer.camera.offset_y = -(HEIGHT / 2.0f);
     renderer.camera.scale = 1.0f;
 
-    glfwSetWindowUserPointer(window, &renderer);
+    glfwSetWindowUserPointer(window, &renderer.camera);
     
     while (!glfwWindowShouldClose(window)) {
+        immediate_quad_centered(&renderer, 0.0f, 0.0f, 150.0f);
+        
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        immediate_quad_centered(&renderer, 0.0f, 0.0f, 150.0f);
-
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(renderer.vertices), renderer.vertices);
-        glDrawElements(GL_TRIANGLES, QUAD_TRIANGLES * QUAD_ELEMENTS, GL_UNSIGNED_INT, renderer.indices);
+        gl_render(&renderer);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &renderer.VAO);
+    glDeleteBuffers(1, &renderer.VBO);
     glDeleteProgram(shader_program);
     
     glfwDestroyWindow(window);
