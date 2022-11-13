a22f563dc83e5f31aabf4a780d9a00591c6950a1
Aiden <marcin.madry@interia.com>
2022-11-13 12:24:23 +0100

    Add useful cl flag

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 REM Change this to your visual studio's 'vcvars64.bat' script path
 set MSVC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
-set CXXFLAGS=/std:c++17 /EHsc /W4 /WX /Zl /wd4996 /wd4201 /nologo %*
+set CXXFLAGS=/std:c++17 /EHsc /W4 /WX /Zl /FC /wd4996 /wd4201 /nologo %*
 set INCLUDES=/I"deps\GLEW\include" /I"deps\GLFW\include"
 set LIBS="deps\GLFW\lib\glfw3.lib" "deps\GLEW\lib\glew32s.lib" opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
